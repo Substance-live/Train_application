c6998178b07d6584435ae000642090fa94edb4f8
Substance <shpion160806@gmail.com>
2024-11-11 11:24:20 +0300

    Edit UI files

--- layouts/__init__.py
+++ layouts/__init__.py
@@ -1,24 +1,14 @@
-from .Delay import *
-from .DriverTable import *
 from .Login import *
-from .Med import *
 from .NewLogin import *
-from .PassangerTable import *
 from .Profile import *
-from .StaffTable import *
 from .Start import *
-from .TableAlter import *
-from .TableStatic import *
+from .Ticket import *
+from .Railcar import *
 
-__all__ = [Delay,
-           DriverTable,
-           Login,
-           Med,
+__all__ = [Login,
            NewLogin,
-           PassangerTable,
            Profile,
-           StaffTable,
            Start,
-           TableAlter,
-           TableStatic,
+           Ticket,
+           Railcar,
            ]
